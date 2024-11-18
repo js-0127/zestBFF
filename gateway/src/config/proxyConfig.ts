415b79ff68df977ff627f028c65eb1a9d4735734
wangshuo31 <wangshuo31@kuaishou.com>
2024-11-18 17:09:45 +0800

    feat: 实现根据配置代理转发

--- gateway/src/config/proxyConfig.ts
+++ gateway/src/config/proxyConfig.ts
@@ -1,13 +1,13 @@
 export default () => ({
-  proxy: [
+  proxyConfig: [
     {
-      target: 'http://localhost:9001', //代理地址
+      target: 'http://localhost:3003', //代理地址
       prefix: '/pc', //前缀
       rewritePrefix: '', //实际请求将pc 替换成 '' 因为后端服务器没有pc这个路由
       httpMethods: ['GET', 'POST'], //允许的请求方式
     },
     {
-      target: 'http://localhost:9002',
+      target: 'http://localhost:9001',
       prefix: '/mobile',
       rewritePrefix: '',
       httpMethods: ['GET', 'POST'],
--- gateway/src/middleware/http-proxy-middleware.ts
+++ gateway/src/middleware/http-proxy-middleware.ts
@@ -1,17 +1,37 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { createProxyMiddleware } from 'http-proxy-middleware';
 import type { Request, Response, NextFunction } from 'express';
+import { ConfigService } from '@nestjs/config';
+
 @Injectable()
 export class HttpProxyMiddleWare implements NestMiddleware {
-  private proxy = createProxyMiddleware({
-    target: 'http://127.0.0.1:3000',
-    pathRewrite: {
-      '/pc': '',
-    },
-    secure: false,
-  });
+  private proxies: any[] = [];
+
+  constructor(private readonly config: ConfigService) {
+    const proxyConfig = this.config.get('proxyConfig');
+    this.proxies = proxyConfig.map((proxy) => {
+      return {
+        prefix: proxy.prefix,
+        middleware: createProxyMiddleware({
+          target: proxy.target,
+          pathRewrite: (path) =>
+            path.replace(proxy.prefix, proxy.rewritePrefix),
+          changeOrigin: true,
+          secure: false,
+        }),
+      };
+    });
+  }
 
   use(req: Request, res: Response, next: NextFunction) {
-    this.proxy(req, res, next);
+
+    const matchingProxy = this.proxies.find((p) =>
+      req.baseUrl.startsWith(p.prefix),
+    );
+    if (matchingProxy) {
+      matchingProxy.middleware(req, res, next);
+    } else {
+      next();
+    }
   }
 }
